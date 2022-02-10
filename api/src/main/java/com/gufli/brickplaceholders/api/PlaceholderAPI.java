37f45121891c5b192c111a5b94a7d95239f7ddf7
Joris Guffens <joris.guffens@outlook.com>
2022-02-10 17:32:26 +0100

    updated api

--- api/src/main/java/com/gufli/brickplaceholders/api/PlaceholderAPI.java
+++ api/src/main/java/com/gufli/brickplaceholders/api/PlaceholderAPI.java
@@ -15,26 +15,38 @@ public class PlaceholderAPI {
         placeholderManager = manager;
     }
 
+    //
+
+    public static PlaceholderManager get() {
+        return placeholderManager;
+    }
+
+    @Deprecated
     public static Component replace(Player player, Component text) {
         return placeholderManager.replace(player, text);
     }
 
+    @Deprecated
     public static Component replace(Player player, String text) {
         return placeholderManager.replace(player, text);
     }
 
+    @Deprecated
     public static Collection<String> placeholders() {
         return placeholderManager.placeholders();
     }
 
+    @Deprecated
     public static void registerReplacer(String key, BiFunction<Player, String, Component> replacer) {
         placeholderManager.registerReplacer(key, replacer);
     }
 
+    @Deprecated
     public static void registerReplacer(String key, Function<Player, Component> replacer) {
         placeholderManager.registerReplacer(key, replacer);
     }
 
+    @Deprecated
     public static void unregisterReplacer(String key) {
         placeholderManager.unregisterReplacer(key);
     }
